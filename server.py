248cfb1c1ea147958a4f39eef60e84e946c0f8e1
amsc101 <anamsollars@hotmail.com>
2017-04-26 16:25:43 +0200

    Update server.py
    Changes in port

--- server.py
+++ server.py
@@ -5,7 +5,10 @@ import socketserver
 # WEB SERVER
 ##
 
-PORT = 9001
+PORT = 8000
+
+socketserver.TCPServer.allow_reuse_adress = True #Para que no tenga que cambiar de puerto
+
 #Handler = http.server.SimpleHTTPRequestHandler --> handler de la biblioteca python
 Handler = web.testHTTPRequestHandler
 
